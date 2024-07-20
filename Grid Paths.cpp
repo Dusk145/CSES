cebe275d83fd36b8495fff12897af2f6bd7481d7
Dusk145 <154054610+Dusk145@users.noreply.github.com>
2024-07-20 14:37:35 +0700

    Update Grid Paths.cpp

--- Grid Paths.cpp
+++ Grid Paths.cpp
@@ -6,50 +6,6 @@
  
 using namespace std;
 
-// TLE
-// bool visited[1001][1001];
-// int dx[2] = {0, 1};
-// int dy[2] = {1, 0};
-// ll cnt = 0;
-
-// void dfs(int n, int i, int j){
-//     if (i == n - 1 && j == n - 1 && visited[i][j]){
-//         ++cnt;
-//         return;
-//     }
-//     for (int k = 0; k < 2; ++k){
-//         int x = i + dx[k];
-//         int y = j + dy[k];
-//         if (x >= 0 && y >= 0 && x < n && y < n && visited[i][j]){
-//             dfs(n, x, y);
-//         }
-//     }
-// }
-
-// int main(){
-//     ios::sync_with_stdio(0);
-//     cin.tie(0);
-//     cout.tie(0);
-//     int n;
-//     cin >> n;
-//     cin.ignore();
-//     char a[n][n];
-//     for (int i = 0; i < n; ++i){
-//         for (int j = 0; j < n; ++j){
-//             cin >> a[i][j];
-//             if (a[i][j] == '*'){
-//                 visited[i][j] = false;
-//             } else {
-//                 visited[i][j] = true;
-//             }
-//         }
-//         cin.ignore();
-//     }
-//     dfs(n, 0, 0);
-//     cout << cnt << endl;
-//     return 0;
-// }
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
