531705619db002a132beb0a692bb6de29a51fc57
Dusk145 <154054610+Dusk145@users.noreply.github.com>
2024-07-20 14:36:45 +0700

    Update Bit Strings.cpp

--- Bit Strings.cpp
+++ Bit Strings.cpp
@@ -6,51 +6,6 @@
  
 using namespace std;
 
-// TLE
-// string res(string a){
-//     for (int i = a.length() - 1; i >= 0; --i){
-//         if (a[i] == '0'){
-//             a[i] = '1';
-//             return a;
-//         } else {
-//             a[i] = '0';
-//         }
-//     }
-//     return a;
-// }
-
-// int main(){
-//     ios::sync_with_stdio(0);
-//     cin.tie(0);
-//     cout.tie(0);
-//     int n;
-//     cin >> n;
-//     string a = "";
-//     while (n){
-//         a.insert(0, "0");
-//         --n;
-//     }
-//     ull cnt = 1;
-//     bool ok = true;
-//     while (ok){
-//         a = res(a);
-//         ++cnt;
-//         cnt %= mod;
-//         bool check = true;
-//         for (int i = 0; i < a.length(); ++i){
-//             if (a[i] == '0'){
-//                 check = false;
-//                 break;
-//             }
-//         }
-//         if (check){
-//             ok = false;
-//         }
-//     }
-//     cout << (ull)(cnt % mod) << endl;
-//     return 0;
-// }
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
